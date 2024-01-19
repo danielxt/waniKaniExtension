34978e8b1dad2bfc7d0a3ba26b727234b1bb2f86
danielxt <danielxt@umich.edu>
2024-01-18 23:08:11 -0500

    basic dash

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,24 +1,49 @@
-async function fetchData() {
-    var apiToken = '7af47b26-693c-436b-825c-2ebc836b6c2f';
-  var apiEndpointPath = 'user';
+
+// valid queries: assignments, level_progressions, resets, reviews, review_statistics, 
+// spaced_repetition_systems, study_materials, subjects, summary, user, voice_actors
+async function fetchData(apiEndpointPath) {
+  var apiToken = '7af47b26-693c-436b-825c-2ebc836b6c2f';
   var requestHeaders =
     new Headers({
-      Authorization: 'Bearer ' + apiToken,
-    });
+    Authorization: 'Bearer ' + apiToken,
+  }); 
   var apiEndpoint =
     new Request('https://api.wanikani.com/v2/' + apiEndpointPath, {
+
       method: 'GET',
       headers: requestHeaders
   });
   const response = await(fetch(apiEndpoint))
   return response.json();
-  
 }
 
+
 async function main() {
-  let json = await fetchData();
-  document.getElementById("username").innerHTML = json['data']['username'];
-  document.getElementById("level").innerHTML = json['data']['level'];
+  // 1. grab data
+  var jsonUser = await fetchData('user');
+  var jsonSummary = await fetchData('summary');
+
+  // 2. query data
+
+  var userName = jsonUser['data']['username']
+  var level = jsonUser['data']['level']
+
+  var availableLessons = jsonSummary['data']['lessons'][0]['subject_ids'].length
+  var availableReviews = jsonSummary['data']['reviews'][0]['subject_ids'].length;
+  
+  var nextReviewsAt = jsonSummary['data']['next_reviews_at']
+  nextReviewsAt = new Date(nextReviewsAt).toLocaleString()
+
+  // 3. send to doc
+
+  document.getElementById("username").innerHTML = userName;
+  document.getElementById("level").innerHTML = level;
+
+  document.getElementById("availableLessons").innerHTML = availableLessons;
+  document.getElementById("availableReviews").innerHTML = availableReviews;
+
+  document.getElementById("nextReviewsAt").innerHTML = nextReviewsAt;
+
 }
 
 main();
